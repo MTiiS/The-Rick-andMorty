7f7510b7f5f0b5de19cd7f086684151aa04c7ba9
MTiiS <kachurvova@rambler.ru>
2021-07-27 12:57:49 +0300

    fix/changed function getCharacters, addCards

--- js/cards_load.js
+++ js/cards_load.js
@@ -1,83 +1,65 @@
-
 let characters = [];
- var episode ;
 
-
-
-function getCharacters(){
-let url  = "https://rickandmortyapi.com/api/character/?page=" + currentPage;  //получаем текущую страницу
-let xhr = new XMLHttpRequest();
-
-
-xhr.open('GET', url);
-xhr.send();
-
-xhr.onload = function() {
-  if (xhr.status != 200) { 
-    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); 
-  } else { 
-    characters = JSON.parse(xhr.response);
-    addCards();
-     
-  }
-};
-}
-
-function getEpisode(url){
-	 return new Promise((resolve, reject) => {
-
-let xhr = new XMLHttpRequest();
-
-
-xhr.open('GET', url);
-xhr.send();
-
-xhr.onload = function() {
-  if (xhr.status != 200) { 
-    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); 
-  } else { 
-   episode = JSON.parse(xhr.response);
-   resolve(episode.name);
-  }
+function getCharacters() {
   
-};
+  let url  = "https://rickandmortyapi.com/api/character/?page=" + currentPage;  //получаем текущую страницу
+  let xhr = new XMLHttpRequest();
 
-});
+  xhr.open('GET', url);
+  xhr.send();
+
+  xhr.onload = function() {
+    if (xhr.status != 200) { 
+      alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); 
+    } else { 
+      characters = JSON.parse(xhr.response);
+      characters = characters.results.map(character => {      //создаем массив персонажей  
+        return {
+          image: character.image,
+          name: character.name,
+          status: character.status,
+          gender: character.gender,
+          location: character.location.name,
+          episode: character.episode[0]
+        };
+      });
+     }
+    //getEpisode();
+    addCards();
+  }; 
 }
 
-
-
-
-function addCards() {
+function addCards() {    //создание карточки персонажа
 
   let card = "";
 
-  for (key in characters.results) {
-  	 let promise = getEpisode(characters.results[key].episode['0']);
-  	console.log((promise));
-
-console.log('1');
-   card += `<div class="list-items">
-     <div class="imageWrapper"><img src="${characters.results[key].image}" alt="${characters.results[key].name}" >  </div>	
-     <div class="contentWrraper">
-     <section> <h1>${characters.results[key].name}</h1>  <ul><li class"red">${characters.results[key].status} - ${characters.results[key].species}<ul></li></p> </section>
-     <section><h4>Last known location: ${characters.results[key].location.name}</h4></section>   
-     <section><h4> First seen in:  </h4></section>
-
-
-
-   </div>
-
-   </div>
-  `
-
+  for (character of characters) {
+    card += `<div class="list-items">
+               <div class="imageWrapper"><img src="${character.image}" alt="${character.name}" > </div>	
+               <div class="contentWrraper">
+                 <section><h1><span class="highlights">${character.name}</span></h1>  <ul><li class"red">${character.status} - ${character.gender}<ul></li></p> </section>
+                 <section><h4><span class="highlights">Last known location:</span> ${character.location}</h4></section>   
+                 <section><h4><span class="highlights">First seen in: </span></h4></section>
+               </div>
+             </div>`
 	}
   
   let items = document.querySelector(".container-items");
   items.innerHTML = card;
-
 }
 
+/*  нужно реализовать получение имени эпизода где впервые встретился персонаж
+function getEpisode(){
+  for (character of characters) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', character.episode);
+    xhr.send();
+   xhr.onload = () => {
+     let episode = JSON.parse(xhr.response).name; 
+    }
+  }
+}*/
 
 getCharacters();
+
 
